4923156960abcd5f7061351575089a7e1691f806
Kevin Ram <kmr232@cornell.edu>
2023-10-19 15:48:32 -0400

    add days/times to list of classes.

--- main.ml
+++ main.ml
@@ -14,44 +14,136 @@ type course = {id: int;
 
 (* CS Courses *)
 let cs_courses = [
+  let classes = [
   {id=1; 
-   name="CS1110"; 
+   name="CS1110";
    description="Introduction to Computing: A Design and Development Perspective"; 
-   schedule={days=["Monday"; "Wednesday"]; time={start=540; finish=630}}};
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=545; finish=595}}};  (* 9:05 to 9:55 *)
   {id=2;
    name="CS1112";
    description="Introduction to Computing: An Engineering and Science Perspective";
-   schedule={days=["Monday"]; time={start=540; finish=630}}};
-  (* {id=3; name="CS1132"; description="Short Course in MATLAB"};
-  {id=4; name="CS1133"; description="Short Course in Python"};
-  {id=5; name="CS1620"; description="Visual Imaging in the Electronic Age"};
-  {id=6; name="CS1710"; description="Introduction to Cognitive Science"};
-  {id=7; name="CS2024"; description="C++ Programming"};
-  {id=8; name="CS2110"; description="Object-Oriented Programming and Data Structures"};
-  {id=9; name="CS2800"; description="Discrete Structures"};
-  {id=10; name="CS2850"; description="Networks"};
-  {id=11; name="CS3110"; description="Data Structures and Functional Programming"};
-  {id=12; name="CS3300"; description="Data-Driven Web Applications"};
-  {id=13; name="CS3410"; description="Computer System Organization and Programming"};
-  {id=14; name="CS4210"; description="Numerical Analysis and Differential Equations"};
-  {id=15; name="CS4320"; description="Introduction to Database Systems"};
-  {id=16; name="CS4321"; description="Practicum in Database Systems"};
-  {id=17; name="CS4410"; description="Operating Systems"};
-  {id=18; name="CS4411"; description="Practicum in Operating Systems"};
-  {id=19; name="CS4420"; description="Computer Architecture"};
-  {id=20; name="CS4620"; description="Introduction to Computer Graphics"};
-  {id=21; name="CS4621"; description="Computer Graphics Practicum"};
-  {id=22; name="CS4700"; description="Foundations of Artificial Intelligence"};
-  {id=23; name="CS4701"; description="Practicum in Artificial Intelligence"};
-  {id=24; name="CS4740"; description="Natural Language Processing"};
-  {id=25; name="CS4750"; description="Foundations of Robotics"};
-  {id=26; name="CS4775"; description="Computational Genetics and Genomics"};
-  {id=27; name="CS4780"; description="Introduction to Machine Learning"};
-  {id=28; name="CS4783"; description="Mathematical Foundations of Machine Learning"};
-  {id=29; name="CS4787"; description="Principles of Large-Scale Machine Learning Systems"};
-  {id=30; name="CS4812"; description="Quantum Information Processing"};
-  {id=31; name="CS4820"; description="Introduction to Analysis of Algorithms"};
-  {id=32; name="CS4860"; description="Applied Logic"}; *)
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=610; finish=660}}};  (* 10:10 to 11:00 *)
+  {id=3; 
+   name="CS1132"; 
+   description="Short Course in MATLAB";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=540; finish=615}}};  (* 9:00 to 10:15 *)
+  {id=4; 
+   name="CS1133"; 
+   description="Short Course in Python";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=540; finish=615}}};  (* 9:00 to 10:15 *)
+  {id=5; 
+   name="CS1620"; 
+   description="Visual Imaging in the Electronic Age";
+   schedule={days=["Wednesday"]; time={start=720; finish=870}}};  (* 12:00 to 2:30 PM *)
+  {id=6; 
+   name="CS1710"; 
+   description="Introduction to Cognitive Science";
+   schedule={days=["Tuesday"]; time={start=900; finish=1050}}};  (* 3:00 to 5:30 PM *)
+  {id=7; 
+   name="CS2024"; 
+   description="C++ Programming";
+   schedule={days=["Thursday"]; time={start=900; finish=1050}}};  (* 3:00 to 5:30 PM *)
+   {id=8; 
+   name="CS2110"; 
+   description="Object-Oriented Programming and Data Structures";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=675; finish=725}}};  (* 11:15 to 12:05 *)
+  {id=9; 
+   name="CS2800"; 
+   description="Discrete Structures";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=545; finish=595}}};  (* 9:05 to 9:55 *)
+  {id=10; 
+   name="CS2850"; 
+   description="Networks";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=610; finish=660}}};  (* 10:10 to 11:00 *)
+  {id=11; 
+   name="CS3110"; 
+   description="Data Structures and Functional Programming";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=675; finish=725}}};  (* 11:15 to 12:05 *)
+  {id=12; 
+   name="CS3300"; 
+   description="Data-Driven Web Applications";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=740; finish=790}}};  (* 12:20 to 1:10 PM *)
+  {id=13; 
+   name="CS3410"; 
+   description="Computer System Organization and Programming";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=805; finish=855}}};  (* 1:25 to 2:15 PM *)
+  {id=14; 
+   name="CS4210"; 
+   description="Numerical Analysis and Differential Equations";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=870; finish=920}}};  (* 2:30 to 3:20 PM *)
+  {id=15; 
+   name="CS4320"; 
+   description="Introduction to Database Systems";
+   schedule={days=["Tuesday"]; time={start=810; finish=960}}};  (* 1:30 to 4:00 PM *)
+  {id=16; 
+   name="CS4321"; 
+   description="Practicum in Database Systems";
+   schedule={days=["Monday"]; time={start=0; finish=1}}};  (* no schedule *)
+  {id=17; 
+   name="CS4410"; 
+   description="Operating Systems";
+   schedule={days=["Wednesday"]; time={start=900; finish=1050}}};  (* 3:00 to 5:30 PM *)
+  {id=18; 
+   name="CS4411"; 
+   description="Practicum in Operating Systems";
+   schedule={days=["Tuesday"]; time={start=0; finish=1}}};  (* no schedule *)
+  {id=19; 
+   name="CS4420"; 
+   description="Computer Architecture";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=810; finish=885}}};  (* 1:30 to 2:45 PM *)
+  {id=20; 
+   name="CS4620"; 
+   description="Introduction to Computer Graphics";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=800; finish=850}}};  (* 1:20 to 2:10 PM *)
+  {id=21; 
+   name="CS4621"; 
+   description="Computer Graphics Practicum";
+   schedule={days=["Monday"]; time={start=0; finish=1}}};  (* no schedule *)
+  {id=22; 
+   name="CS4700"; 
+   description="Foundations of Artificial Intelligence";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=900; finish=975}}};  (* 3:00 to 4:15 PM *)
+  {id=23; 
+   name="CS4701"; 
+   description="Practicum in Artificial Intelligence";
+   schedule={days=["Wednesday"]; time={start=0; finish=1}}};  (* no schedule *)
+  {id=24; 
+   name="CS4740"; 
+   description="Natural Language Processing";
+   schedule={days=["Monday"]; time={start=780; finish=930}}};  (* 1:00 to 3:30 PM *)
+  {id=25; 
+   name="CS4750"; 
+   description="Foundations of Robotics";
+   schedule={days=["Friday"]; time={start=780; finish=930}}};  (* 1:00 to 3:30 PM *)
+  {id=26; 
+   name="CS4775"; 
+   description="Computational Genetics and Genomics";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=840; finish=915}}};  (* 2:00 to 3:15 PM *)
+  {id=27; 
+   name="CS4780"; 
+   description="Machine Learning for Intelligent Systems";
+   schedule={days=["Monday"; "Wednesday"]; time={start=610; finish=685}}};  (* 10:10 to 11:25 PM *)
+  {id=28; 
+   name="CS4786"; 
+   description="Machine Learning for Data Science";
+   schedule={days=["Tuesday"; "Thursday"]; time={start=610; finish=685}}};  (* 10:10 to 11:25 PM *)
+  {id=29; 
+   name="CS4810"; 
+   description="Introduction to Computer Vision";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=675; finish=725}}};  (* 11:15 to 12:05 *)
+  {id=30; 
+   name="CS4812"; 
+   description="Quantum Information Processing";
+   schedule={days=["Wednesday"]; time={start=965; finish=1040}}};  (* 4:05 to 5:20 PM *)
+  {id=31; 
+   name="CS4820"; 
+   description="Introduction to Analysis of Algorithms";
+   schedule={days=["Monday"; "Wednesday"; "Friday"]; time={start=545; finish=595}}};  (* 9:05 to 9:55 *)
+  {id=32; 
+   name="CS4860"; 
+   description="Applied Logic";
+   schedule={days=["Friday"]; time={start=965; finish=1040}}};  (* 4:05 to 5:20 PM *)
+  ]
 ]
 
 (* displays all available CS courses *)
